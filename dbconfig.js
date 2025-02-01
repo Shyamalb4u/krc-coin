dade16ed10d78be6ceb20d4a8eac20ded5f5d33c
Shyamal <info@bigsss.com>
2025-02-01 18:03:20 +0530

    dbconfig change

---
+++ dbconfig.js
@@ -0,0 +1,14 @@
+const { prototype } = require("jsonwebtoken/lib/JsonWebTokenError");
+
+const config = {
+  user: "krc_coin_user", // Database username
+  password: "KRC597Cn*Dat", // Database password
+  server: "78.47.118.224", // Server IP address
+  database: "krc_coin_db", // Database name
+  options: {
+    encrypt: false, // Disable encryption
+  },
+  port: 1533,
+};
+
+module.exports = config;
