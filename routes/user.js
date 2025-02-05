e573281afe7e465b88e013349a5cfe576e294b02
Shyamal <info@bigsss.com>
2025-02-05 15:07:39 +0530

    Pending Activation Uploaded

--- routes/user.js
+++ routes/user.js
@@ -7,6 +7,10 @@ router.get("/getUser/:id", userController.getUser);
 router.get("/getMail/:mail", userController.getMail);
 router.get("/getPhrases/:phrases", userController.getPhrases);
 router.get("/getDirect/:uid", userController.getDirect);
+router.get(
+  "/pending_activation/:publicKey",
+  userController.getPendingActivation
+);
 router.get("/getDirectSummery/:uid", userController.getDirectSummery);
 router.get("/getall", userController.getall);
 router.post("/signup", userController.signup);
